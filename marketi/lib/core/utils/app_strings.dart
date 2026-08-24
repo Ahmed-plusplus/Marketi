af180970fc939b51632505fe84940b990e5e5714
AMostafa <amostafa@zahatechs.com>
2026-08-24 10:01:29 +0300

    add all text strings

--- marketi/lib/core/utils/app_strings.dart
+++ marketi/lib/core/utils/app_strings.dart
@@ -11,21 +11,23 @@ abstract class AppStrings {
   static const forgetPasswordTitle = 'Congratulations';
   static const forgetPasswordBody1 = 'Please enter your phone number to receive a verification code';
   static const forgetPasswordBody2 = 'Please enter your email address to receive a verification code';
-  static const forgetPasswordBody3 = 'Please enter the 4 digit code\nsent to: ';
-  static const forgetPasswordBody4 = 'New password must be\ndifferent from last password';
-  static const forgetPasswordBody5 = 'You have updated the password. please\nlogin again with your latest password';
+  static const forgetPasswordBody3 = 'Please enter the 4 digit code sent to: ';
+  static const forgetPasswordBody4 = 'New password must be different from last password';
+  static const forgetPasswordBody5 = 'You have updated the password. please login again with your latest password';
 
   ///Cart Texts
   static const emptyCartTitle = 'Your Cart is Empty';
-  static const emptyCartBody = 'Check our big offers, fresh products\nand fill your cart with items';
+  static const emptyCartBody = 'Check our big offers, fresh products and fill your cart with items';
 
   ///Checkout Texts
-  static const checkoutTitle = 'Thank You!';
-  static const checkoutBody = 'Woah, You Have sucessfully orderd';
+  static const checkoutTitle1 = 'Thank You!';
+  static const checkoutBody1 = 'Woah, You Have sucessfully orderd';
+  static const checkoutTitle2 = 'You\'re Not Signed In';
+  static const checkoutBody2 = 'Please sign in and enjoy our services and big offers';
 
   ///Search Texts
   static const notFoundSearchTitle = 'Oops Not Found!';
-  static const notFoundSearchBody = 'Check our big offers, fresh products\nand fill your cart with items';
+  static const notFoundSearchBody = 'Check our big offers, fresh products and fill your cart with items';
 
   ///Buttons
   static const next = 'Next';
@@ -38,23 +40,48 @@ abstract class AppStrings {
   static const startShopping = 'Start Shopping';
   static const goHome = 'Go Home';
   static const continueShopping = 'Continue Shopping';
+  static const add = 'Add';
+  static const apply = 'Apply';
+  static const placeOrder = 'Place Order';
+  static const addToCart = 'Add to Cart';
 
   ///Text Links
   static const forgetPassword = 'Forgot Password?';
   static const register = 'register?';
   static const tryAnotherWay = 'Try Another Way';
   static const resendCode = 'Resend Code';
+  static const viewAll = 'View all';
+  static const change = 'Change';
+  static const seeMore = 'See more';
+  static const cancel = 'Cancel';
+  static const ok = 'Ok';
 
   ///Headers
   static const forgetPasswordHeader = 'Forgot Password';
   static const verificationCodeHeader = 'Verification Code';
   static const cartHeader = 'Cart';
   static const checkoutHeader = 'Checkout';
+  static const popularProductHeader = 'Popular Product';
+  static const buyAgainHeader = 'Buy Again';
+  static const bestForYouHeader = 'Best for You';
+  static const favoritesHeader = 'Favorites';
+  static const categoriesHeader = 'Categories';
+  static const categoryHeader = 'Category';
+  static const brandsHeader = 'Brands';
+  static const productDetailsHeader = 'Product Details';
+  static const myProfileHeader = 'My Profile';
+  static const myOrdersHeader = 'My Orders';
+  static const paymentsHeader = 'Payments';
+  static const accountSettingsHeader = 'Account Settings';
+  static const supportHeader = 'Support';
 
   ///Texts
   static const loginWith = 'Or Continue With';
   static const rememberMe = 'Remember Me';
   static const newUser = 'Are you new in Marketi ';
+  static const searchHistory = 'Search History ';
+  static const cash = 'Cash on delivery';
+  static const welcome = 'Welcome,';
 
   ///Titles
   static const yourName = 'Your Name';
@@ -63,6 +90,25 @@ abstract class AppStrings {
   static const email = 'Email';
   static const password = 'Password';
   static const confirmPassword = 'Confirm Password';
+  static const allProducts = 'All Products';
+  static const address = 'Address';
+  static const home = 'Home';
+  static const deliveryTime = 'Delivery time';
+  static const payment = 'Payment';
+  static const total = 'Total';
+  static const supTotal = 'Suptotal';
+  static const deliveryFees = 'Delivery Fees';
+  static const productValue = 'Product Value';
+  static const selectSize = 'Select Size';
+  static const price = 'Price';
+  static const accountPreference = 'Account Preferences';
+  static const subscriptionPayment = 'Subscription & Payment';
+  static const appNotification = 'App Notifications';
+  static const darkMode = 'Dark Mode';
+  static const rateUs = 'Rate Us';
+  static const provideFeedback = 'Provide Feedback';
+  static const logout = 'Log Out';
+  static const rateUsDialogTitle = 'How would you rate us ?';
 
   ///Hints
   static const usernameOrEmailHint = 'Username or Email';
@@ -70,6 +116,7 @@ abstract class AppStrings {
   static const phoneNoHint = '+20 1501142409';
   static const emailHint = 'You@gmail.com';
   static const searchHint = 'What are you looking for ?';
+  static const voucherCodeHint = 'Voucher code';
 
-
+  static hi(String name) => 'Hi $name !';
 }
